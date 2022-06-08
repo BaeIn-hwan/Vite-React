d96fe8f73dc1d6a5d7b0c1cb42d92d3849ad6fda
Bae InHwan <ih9137@naver.com>
2022-06-08 15:08:48 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+const path = require('path')
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 interface ConfigOptions {
   command: string;
