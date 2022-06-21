cd58706f8f1b5b443510667729035a6e38d9df2b
BaeInhwan <ihbae@snack24h.com>
2022-06-21 10:21:33 +0900

    [main] 린트 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,58 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true
-	},
-	extends: [
-		"prettier",
-		"prettier/react",
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true
-		},
-		ecmaVersion: 'latest',
-		sourceType: module
-	},
-	plugins: [
-		'react',
-		'@typescript-eslint'
-	],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',		
-	}
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    // 'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    parser: 'babel-eslint',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  /*
+    eslint 설정 시 off(0), warn(1), error(2) 문자열로 작성한다.
+  */
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': [
+      'warn', {
+        'ignoreRestArgs': false
+      }
+    ],
+    // 'prettier/prettier': [
+    //   'error', {
+    //     'arrowParens': 'avoid',
+    //     'bracketSpacing': true,
+    //     'insertPragma': false,
+    //     'jsxBracketSameLine': false,
+    //     'jsxSingleQuote': true,
+    //     'printWidth': 80,
+    //     'singleQuote': false,
+    //     'semi': true,
+    //     'useTabs': false,
+    //     'tabWidth': 2,
+    //     'trailingComma': 'all'
+    //   }
+    // ]
+  },
 };
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,86 +1,40 @@
 module.exports = {
   extends: [
     'stylelint-order',
+    // 'stylelint-config-standard',
     'stylelint-config-standard-scss',
   ],
   plugins: [
     'stylelint-scss',
-    'stylelint-order'
+    'stylelint-order',
   ],
   rules: {
-    'no-eol-whitespace': true,
-    'at-rule-no-unknown': null,
-    'string-quotes': 'single',
-    'comment-no-empty': null,
-    'custom-property-pattern': null,
-    'function-no-unknown': null,
-    'declaration-block-trailing-semicolon': 'always',
-    'no-invalid-position-at-import-rule': [
-      true,
-      {
-        ignoreAtRules: ['use', 'forward'],
-      },
-    ],
-    'scss/at-mixin-named-arguments': 'always',
-    'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-else-closing-brace-space-after': 'always-intermediate',
-    'scss/at-else-empty-line-before': 'never',
-    'scss/at-else-if-parentheses-space-before': 'always',
-    'scss/at-extend-no-missing-placeholder': true,
-    'scss/at-function-parentheses-space-before': 'never',
-    'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-if-closing-brace-space-after': 'always-intermediate',
-    'scss/at-if-no-null': true,
-    'scss/at-import-no-partial-leading-underscore': true,
-    'scss/at-mixin-argumentless-call-parentheses': 'never',
-    'scss/at-mixin-parentheses-space-before': 'never',
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['screen']
-      }
-    ],
-    'scss/comment-no-empty': [
-      true,
-      {
-        severity: 'warning'
-      }
-    ],
-    'scss/declaration-nested-properties-no-divided-groups': true,
-    'scss/dollar-variable-colon-newline-after': null,
-    'scss/dollar-variable-colon-space-after': 'always-single-line',
-    'scss/dollar-variable-colon-space-before': 'never',
-    'scss/dollar-variable-no-missing-interpolation': true,
-    'scss/double-slash-comment-whitespace-inside': 'always',
-    'scss/function-color-relative': [
-      true,
-      {
-        severity: 'warning',
-        message: 'Use the new scss color functions instead!',
-      },
-    ],
-    'scss/media-feature-value-dollar-variable': [
+    'at-rule-empty-line-before': [
       'always',
       {
-        severity: 'warning',
-        ignore: ['keywords']
-      },
+        except: ['blockless-after-blockless'],
+        ignore: ['blockless-after-blockless'],
+        ignoreAtRules: ['include']
+      }
     ],
-    'scss/no-duplicate-mixins': true,
-    'scss/no-global-function-names': null,
-    'scss/operator-no-newline-after': null,
-    'scss/operator-no-newline-before': true,
-    'scss/operator-no-unspaced': null,
-    'scss/selector-no-redundant-nesting-selector': true,
+    'no-empty-source': true,
     'order/order': [
-      'custom-properties',
+			'custom-properties',
       'declarations'
-    ],
+		],
     'order/properties-order': [
       'content',
       'display',
+      'flex-direction',
+      'flex-wrap',
+      'flex-basis',
+      'flex-grow',
+      'flex-shrink',
       'justify-content',
       'align-items',
+      'align-content',
+      'flex',
+      'flex-flow',
       'overflow',
       'position',
       'z-index',
@@ -89,11 +43,10 @@ module.exports = {
       'bottom',
       'left',
       'float',
-      'clear',
-      'width',
+			'width',
       'min-width',
-      'max-width',
-      'height',
+      'max-height',
+			'height',
       'min-height',
       'max-height',
       'margin',
@@ -113,10 +66,14 @@ module.exports = {
       'border-left',
       'border-radius',
       'background',
-      'box-shadow',
-      'font-family',
+      'background-color',
+      'background-image',
+      'background-repeat',
+      'background-position',
+      'background-size',
       'font-size',
       'font-weight',
+      'font-family',
       'font-style',
       'color',
       'text-align',
@@ -124,20 +81,21 @@ module.exports = {
       'vertical-align',
       'opacity',
       'cursor',
-      'box-sizing',
-    ],
+      'clear',
+      'transform',
+      'transition',
+      'clip',
+		],
     'property-no-vendor-prefix': [true, {
       ignoreProperties: ['text-size-adjust', 'appearance', 'display']
     }],
-    'value-no-vendor-prefix': [true, {
-      ignoreValues: ['box']
-    }],
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        except: ['inside-block'],
-        ignoreAtRules: ['import', 'include']
+    'selector-class-pattern': '^[a-zA-Z]',
+    'selector-class-pattern': '^[a-zA-Z]',
+    'string-quotes': 'single',
+    'value-no-vendor-prefix': [
+      true, {
+        ignoreValues: ['box', 'text-size-adjust']
       }
     ],
-  },
+  }
 }
